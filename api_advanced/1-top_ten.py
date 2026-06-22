c459c10f156906524544f9217d3193cbc9cb649b
umaureen <u.maureen@alustudent.com>
2026-06-22 13:59:32 +0200

    read

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,19 +4,21 @@ import requests
 
 
 def top_ten(subreddit):
-    """Print titles of first 10 hot posts for a given subreddit."""
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
+
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
         json_data = response.json()
-        children = json_data.get('data', {}).get('children', [])
-        
-        if children:
-            for i in range(min(10, len(children))):
-                print(children[i].get('data', {}).get('title'))
-        else:
-            print(None)
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
     else:
         print(None)
