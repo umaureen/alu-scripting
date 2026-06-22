5caebc64880ceb683d88b44136d311a1482a8cbe
umaureen <u.maureen@alustudent.com>
2026-06-22 11:43:07 +0000

    read

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,22 +5,21 @@ import requests
 
 def top_ten(subreddit):
     """Print titles of first 10 hot posts for a given subreddit."""
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-    }
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, headers=headers)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            children = data.get('data', {}).get('children', [])
-            
-            if children:
-                for child in children[:10]:
-                    print(child.get('data', {}).get('title'))
-                return
-        except Exception:
-            pass
-    
-    print(None)
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()
+    posts = data.get('data', {}).get('children', [])
+
+    if not posts:
+        print(None)
+        return
+
+    for i in range(10):
+        if i < len(posts):
+            print(posts[i]['data']['title'])
